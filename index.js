e00ec7cedb11887bcf2aa59e858242c1d654905c
Younhee Lee <benjaminlee.kr@gmail.com>
2019-11-13 14:06:06 -0800

    1. created login router \n 2. add loginRouter in the middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let express = require("express");
 let bodyParser = require("body-parser");
 let path = require("path");
 let expressHbs = require("express-handlebars");
+let loginRouter = require("./routes/login")
 
 let app = express();
 
@@ -23,9 +24,8 @@ app.set("view engine", "hbs");
 app.set("views", "views");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(loginRouter);
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+
 
 app.listen(port, () => console.log(`Server is listening on: ${port}`));
