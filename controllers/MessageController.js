628664a8075e593b4c2edec93f1683e2b825af06
Younhee Lee <benjaminlee.kr@gmail.com>
2019-11-18 00:52:22 -0800

    1. date fix

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -9,9 +9,23 @@ exports.messagePage = async(req, res) => {
     let messageList = await messageModel.getMessages().then(([rows, fieldData]) => {
         return rows;
     });
-    console.log(talkList);
+    let refinedList = new Array();
+    let eachDay = new Array();
+    let tmpDate = messageList[0].created_at.slice(8,10);
+    eachDay.push(messageList[0]);
+    for(let i = 1; i < messageList.length; i++){
+        if(tmpDate != messageList[i].created_at.slice(8,10)){
+            refinedList.push(eachDay);
+            eachDay = [];
+            tmpDate = messageList[i].created_at.slice(8,10)
+        } else {
+            eachDay.push(messageList[i]);
+        }
+    }
+    refinedList.push(eachDay);
 
+    console.log(refinedList[1]);
     
-    res.render('message', { chatList: talkList, messageList: messageList, messageCSS: true }); 
+    res.render('message', { chatList: talkList, messageList: refinedList, messageCSS: true }); 
 };
 
