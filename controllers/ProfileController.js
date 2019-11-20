5ce9a95fe052f4d8a99be91417bdf4b6d51ca1ce
Joey Tang <joeyt@sfu.ca>
2019-11-19 17:43:45 -0800

    Added posts partial to profile, tweaked posts styling, profile UI is done.

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -2,11 +2,16 @@ let userModel = require('../models/user');
 let postModel = require("../models/post");
 
 // TODO: Add number of posts to user table or do seperate query on post table
-exports.getProfile = (req, res) => {
-    userModel.getUserById(req.params.userId)
+async function getProfile(req, res) {
+    await userModel.getUserById(req.params.userId)
     .then(([rows, field]) => {
-        console.log(rows);
-        res.render('userProfile', {profileCSS: true, user: rows[0] });
+        postModel.getPostsByUserId(rows[0].id)
+        .then(([postrows, field]) => {
+            console.log(postrows);
+            res.render('userProfile', {profileCSS: true, postCSS: true, user: rows[0], posts: postrows });
+        }).catch(error => console.log("get user posts error: " + error));
     })
     .catch(error => console.log("get profile error: " + error));
 };
+
+module.exports.getProfile = getProfile;
