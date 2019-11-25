6414906e2c672f3ba64b4cfd7d378e00c909ed94
Younhee Lee <benjaminlee.kr@gmail.com>
2019-11-25 15:27:44 -0800

    1.fix non-valid date bug  2. fixed wrong page rendering after sending message

--- routes/message.js
+++ routes/message.js
@@ -2,7 +2,19 @@ let express = require("express");
 let router = express.Router();
 let messageController = require("../controllers/MessageController");
 
-router.get("/list/:userId", messageController.messagePage);
-router.get("/list/:userId/:topicId", messageController.messagePage);
-router.post("/list/:userId/:topicId/send", messageController.messageSend);
+const checkuser = (req, res, next) => {
+    let currentUser = req.session.user.id
+    let requestUser = req.params.userId
+
+    console.log(req);
+    // console.log(requestUser);
+    if(currentUser != requestUser){
+       return res.redirect("/messages/list/"+currentUser)
+    }
+    next()
+}
+
+router.get("/list/:userId", checkuser, messageController.messagePage);
+router.get("/list/:userId/:topicId", checkuser, messageController.messagePage);
+router.post("/list/:userId/:topicId/send", checkuser, messageController.messageSend);
 module.exports = router;
